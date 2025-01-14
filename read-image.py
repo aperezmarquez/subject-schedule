a26e64f8eeea5500b0520bb5ae42c1203f8a0957
aperezmarquez <aperezmarquez.business@gmail.com>
2025-01-14 16:06:22 +0100

    Added: get_critical_points(), check_subject()
    The crop function now crops all columns into 6 rows, one per subject time, critical points get all the sizes we need to crop the image and check subject gets the hours for the classes and prints the text (still to implement checking if read correctly)

--- read-image.py
+++ read-image.py
@@ -2,35 +2,72 @@
 import pytesseract as tsr
 from PIL import Image
 
-def read_subjects(fileName, day):
-    img = Image.open(fileName)
-    width, height = img.size
-    
-    crop_subject(img, width, height, day)
-
-def crop_subject(img, width, height, day):
+# Gets all the critical points of the img
+def get_critical_points(width, height):
     # Remove subject times and days of the week
     left_img_error = width*0.04
     top_img_error = height*0.1
-    # Get the column width
+    # Get column width and row height
     column_width = (width - left_img_error) / 5
+    row_height = (height - top_img_error) / 6
 
-    # Calculate the top left corner where we want to crop
-    x_start_point = left_img_error + (day*column_width)
-    y_start_point = top_img_error
+    return (left_img_error, top_img_error, column_width, row_height)
 
-    # Calculate the bottom right corner
-    x_end_point = x_start_point + column_width
-    y_end_point = y_start_point + height
+def check_subject(subject_row, subject_info):
+    start_hour = 7 + subject_row*2
+    end_hour = start_hour + 2
 
-    coordenadas = (x_start_point, y_start_point, x_end_point, y_end_point)
-    img_cropped = img.crop(coordenadas)
+    lines = subject_info.splitlines()
+    lines = [l for l in lines if l.strip()]
+    if (len(lines) < 5):
+        return
+
+    i=0
+    if (lines[1].replace(".", "").replace(":", "").replace("0", "").strip().isdigit()):
+        print("change")
+        i += 1
+
+    print(lines[i])
+    print(lines[i+1])
+    print(lines[i+2])
+    print(lines[i+3])
+    print(lines[i+4])
+    print("\n")
+
+# Reads all the subjects of the calendar
+def read_subjects(fileName):
+    img = Image.open(fileName)
+    width, height = img.size
     
-    text = tsr.image_to_string(img_cropped, lang='spa')
-    print(text)
+    subject = {}
+
+    # Sacamos los puntos criticos
+    left_img_error, top_img_error, column_width, row_height = get_critical_points(width, height)
+
+    for i in range(0, 6):
+        crop_subject(img, left_img_error, top_img_error, column_width, row_height, i)
+        
+
+def crop_subject(img, left_img_error, top_img_error, column_width, row_height, day):
+    # Calculate left and right x values where to crop
+    x_start_point = left_img_error + (day*column_width)
+    x_end_point = x_start_point + column_width
+
+    # Get every class in the day
+    for j in range(0, 6):
+        # Calculate top and bottom y values
+        y_start_point = top_img_error + (j * row_height)
+        y_end_point = y_start_point + (j + 1 * row_height)
+
+        # Asign coordinates and crop the img
+        coordinates = (x_start_point, y_start_point, x_end_point, y_end_point)
+        img_cropped = img.crop(coordinates)
+    
+        text = tsr.image_to_string(img_cropped, lang='spa')
+        check_subject(j, text)
 
 def convert_to_json(fileName, day):
     pass
 
 if __name__ == '__main__':
-    read_subjects('MAIS2_segundo_cuatri.png', 1)
+    read_subjects('MAIS2_segundo_cuatri.png')
