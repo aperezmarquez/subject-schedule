45980552323d661808f5d60da715b0ac6b342fc6
aperezmarquez <aperezmarquez.business@gmail.com>
2025-01-14 19:15:15 +0100

    Added: functionalities to read_image.py
    All the code converts the schedule to a json

--- read-image.py
+++ read-image.py
@@ -1,6 +1,7 @@
 # Read all the subjects
 import pytesseract as tsr
 from PIL import Image
+import json
 
 # Gets all the critical points of the img
 def get_critical_points(width, height):
@@ -13,6 +14,7 @@ def get_critical_points(width, height):
 
     return (left_img_error, top_img_error, column_width, row_height)
 
+# Checks and cleans the subject
 def check_subject(subject_row, subject_info):
     start_hour = 9 + subject_row*2
     end_hour = start_hour + 2
@@ -22,7 +24,9 @@ def check_subject(subject_row, subject_info):
     lines = [l for l in lines if l.strip()]
     # If the length is smaller than 1, then there is no subject
     if (len(lines) < 1):
-        return
+        return -1
+    elif (len(lines) < 4):
+        return -2
 
     # We don't need the times of the class, so we remove them
     if (lines[0].replace(".", "").replace(":", "").strip().isdigit()):
@@ -31,34 +35,21 @@ def check_subject(subject_row, subject_info):
     if (lines[-1].replace(".", "").replace(":", "").strip().isdigit()):
         del lines[-1]
 
-    print(str(start_hour) + " - " + str(end_hour) + ": ")
-    print(lines[0])
-    print(lines[1])
-    print(lines[2])
-    print(lines[3])
-    print("\n")
+    result = convert_to_json(lines, start_hour, end_hour)
 
-# Reads all the subjects of the calendar
-def read_subjects(fileName):
-    img = Image.open(fileName)
-    width, height = img.size
-    
-    subject = {}
-
-    # Sacamos los puntos criticos
-    left_img_error, top_img_error, column_width, row_height = get_critical_points(width, height)
-
-    for i in range(0, 5):
-        crop_subject(img, left_img_error, top_img_error, column_width, row_height, i)
+    return result
         
-
+# Crops the img into subjects
 def crop_subject(img, left_img_error, top_img_error, column_width, row_height, day):
     # Calculate left and right x values where to crop
     x_start_point = left_img_error + (day*column_width)
     x_end_point = x_start_point + column_width
 
+    day_subjects = []
+
     # Get every class in the day
     for j in range(0, 6):
+        
         # Calculate top and bottom y values
         y_start_point = top_img_error + (j * row_height)
         y_end_point = y_start_point + (j + 1 * row_height)
@@ -69,12 +60,47 @@ def crop_subject(img, left_img_error, top_img_error, column_width, row_height, d
 
         # Saves all cropped images
         #img_cropped.save('images/subject_' + str(day) + '_' + str(j) + '.png')
-    
-        text = tsr.image_to_string(img_cropped, lang='spa')
-        check_subject(j, text)
 
-def convert_to_json(fileName, day):
-    pass
+        text = tsr.image_to_string(img_cropped, lang='spa')
+        clean_subject = check_subject(j, text)
+
+        if (clean_subject != -1 and clean_subject != -2):
+            day_subjects.append(clean_subject)
+
+    return day_subjects
+
+
+# Reads all the subjects of the calendar
+def read_subjects(fileName):
+    img = Image.open(fileName)
+    width, height = img.size
+
+    week_subjects = {}
+
+    # Sacamos los puntos criticos
+    left_img_error, top_img_error, column_width, row_height = get_critical_points(width, height)
+
+    for i in range(0, 5):
+        day_subjects = crop_subject(img, left_img_error, top_img_error, column_width, row_height, i)
+        
+        week_subjects[i+1] = day_subjects
+
+    week_subjects = json.dumps(week_subjects, indent=4)
+
+    return week_subjects
+
+
+# Converts the text read from the img to json
+def convert_to_json(lines, start_hour, end_hour):
+    subject = {
+        "start_hour": start_hour,
+        "course": lines[0],
+        "subject": lines[1],
+        "teacher": lines[2],
+        "end_hour": end_hour,
+    }
+
+    return subject
 
 if __name__ == '__main__':
     read_subjects('MAIS2_segundo_cuatri.png')
